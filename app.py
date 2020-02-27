b02fe9ec4c9f0fe02b01296ba44ee6472c782b0a
Trung Minh <trungminh@Trungs-iMac.local>
2020-02-27 19:43:31 +0700

    set dynamic layout

--- app.py
+++ app.py
@@ -29,33 +29,35 @@ server = flask.Flask(__name__)
 server.secret_key = os.environ.get('secret_key', str(randint(0, 1000000)))
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, server=server)
 
-app.layout = html.Div([
-                 html.H1(children = 'Scraper Demo'),
+def serve_layout():
+    return html.Div([
+                     html.H1(children = 'Scraper Demo'),
+    
+                     html.Div([
+                         html.Div(dcc.Input(id = 'input_text', type = 'text',
+                                            style ={ 'width': 600, 'margin': 10}),
+                                  style = {'display': 'inline-block', 'fontColor': 'blue'}),
+                         html.Button('Search', id='button_search'),
+                         ], style = {'margin-bottom': 10}),
 
-                 html.Div([
-                     html.Div(dcc.Input(id = 'input_text', type = 'text',
-                                        style ={ 'width': 600, 'margin': 10}),
-                              style = {'display': 'inline-block', 'fontColor': 'blue'}),
-                     html.Button('Search', id='button_search'),
-#                     html.Div(dcc.Input(id = 'input_totaltime', type = 'number'), style={'display': 'inline-block'}),
-                     ], style = {'margin-bottom': 10}),
-                 html.Div(                 
-                     html.A('Download Data', id = 'link_download',
-                            download = 'rawdata.csv', href='', target='_blank')
-                     ),
-                              
-                 html.Div([
-                     dt.DataTable(id = 'dtable_time',
-                         columns = [{'name': i, 'id': i} for i in ['Title', 'Email', 'Phone']],
-                         style_table = {'overflowX': 'scroll'},
-                         style_cell = {'textAlign': 'left',
-                                     'height': 'auto',
-                                     'whiteSpace': 'normal'
-                                     })
-                     ], style = {'display': 'inline-block', 'width':1000}),
-                                              
-                 ], style = {'textAlign': 'center'})
+                     html.Div(                 
+                         html.A('Download Data', id = 'link_download',
+                                download = 'rawdata.csv', href='', target='_blank')
+                         ),
+                                  
+                     html.Div([
+                         dt.DataTable(id = 'dtable_time',
+                             columns = [{'name': i, 'id': i} for i in ['Title', 'Email', 'Phone']],
+                             style_table = {'overflowX': 'scroll'},
+                             style_cell = {'textAlign': 'left',
+                                         'height': 'auto',
+                                         'whiteSpace': 'normal'
+                                         })
+                         ], style = {'display': 'inline-block', 'width':1000}),
+                                                  
+                     ], style = {'textAlign': 'center'})
 
+app.layout = serve_layout
                      
 @app.callback(Output('button_search', 'disabled'),
               [Input('input_text', 'value')])
